af156f09583bd4faa877e8ba2a443710f84f56fe
Erick Gomes Hostin <erickhescola@gmail.com>
2018-11-12 11:25:16 -0200

    parte template index

--- stocker_project/stocker/views/index.py
+++ stocker_project/stocker/views/index.py
@@ -2,5 +2,4 @@ from django.shortcuts import render
 
 
 def index(request):
-	context_dict = {'boldmessage': "stocker!!"}
-	return render(request, 'stocker/index.html', context=context_dict)
+	return render(request, 'stocker/index.html')
